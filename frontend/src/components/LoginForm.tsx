eeab425d95a83a1504f119a0d25da1bae7bf7014
Iafcy622 <frankiecheung512@gmail.com>
2025-03-21 18:36:29 +0800

    Added validation logic and helper text

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -9,11 +9,16 @@ import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import FormHelperText from '@mui/material/FormHelperText';
 
 export default function LoginForm() {
   const [showPassword, setShowPassword] = React.useState<boolean>(false);
   const [email, setEmail] = React.useState<string>('');
   const [password, setPassword] = React.useState<string>('');
+
+  const [disableSubmit, setDisableSubmit] = React.useState<boolean>(true);
+  const [emailErrorMessage, setEmailErrorMessage] = React.useState<string | null>(null);
+  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState<string | null>(null);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
   const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -23,8 +28,56 @@ export default function LoginForm() {
     event.preventDefault();
   };
 
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+    if (!email) {
+      setEmailErrorMessage("Email is required.");
+    } else if (!emailRegex.test(email)) {
+      setEmailErrorMessage("Please enter a valid email address.");
+    } else {
+      setEmailErrorMessage(null);
+      return true;
+    }
+
+    return false;
+  };
+
+  const validatePassword = (password: string) => {
+    const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).*$/;
+
+    if (!password) {
+      setPasswordErrorMessage("Password is required.");
+    } else if (password.length < 8 || password.length > 24) {
+      setPasswordErrorMessage("Password must be 8-24 characters long.");
+    } else if (!passwordRegex.test(password)) {
+      setPasswordErrorMessage("Password must include at least 1 lowercase character, uppercase character, symbol and number.");
+    } else {
+      setPasswordErrorMessage(null);
+      return true;
+    }
+
+    return false;
+  }
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    const isValid = validateEmail(e.target.value);
+    setDisableSubmit(!isValid || passwordErrorMessage !== null);
+  }
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+    const isValid = validatePassword(e.target.value);
+    setDisableSubmit(emailErrorMessage !== null || !isValid);
+  }
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (validateEmail(email) && validatePassword(password)) {
+      
+    }
   }
 
   return (
@@ -34,16 +87,20 @@ export default function LoginForm() {
       onSubmit={handleSubmit}
     >
       <TextField
+        required
         id="login-email"
         label="Email"
         variant="outlined"
         fullWidth={true}
         value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={handleEmailChange}
+        error={emailErrorMessage !== null}
+        helperText={emailErrorMessage}
         sx={{ mb: 2 }}
       />
       
       <FormControl
+        required
         variant="outlined"
         fullWidth={true}
         sx={{ mb: 2 }}
@@ -53,7 +110,8 @@ export default function LoginForm() {
           id="login-password"
           type={showPassword ? 'text' : 'password'}
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={handlePasswordChange}
+          error={passwordErrorMessage !== null}
           endAdornment={
             <InputAdornment position="end">
               <IconButton
@@ -71,11 +129,15 @@ export default function LoginForm() {
           }
           label="Password"
         />
+        {passwordErrorMessage !== null && (
+          <FormHelperText error={true}>{passwordErrorMessage}</FormHelperText>
+        )}
       </FormControl>
 
       <Button
         type="submit"
         variant="contained"
+        disabled={disableSubmit}
         sx={{ width: '100%', mt: 2 }}
       >
         Login
--- frontend/src/components/SignupForm.tsx
+++ frontend/src/components/SignupForm.tsx
@@ -9,6 +9,7 @@ import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import FormHelperText from '@mui/material/FormHelperText';
 
 export default function LoginForm() {
   const [showPassword, setShowPassword] = React.useState<boolean>(false);
@@ -16,6 +17,11 @@ export default function LoginForm() {
   const [email, setEmail] = React.useState<string>('');
   const [password, setPassword] = React.useState<string>('');
   const [passwordConfirm, setPasswordConfirm] = React.useState<string>('');
+
+  const [disableSubmit, setDisableSubmit] = React.useState<boolean>(true);
+  const [emailErrorMessage, setEmailErrorMessage] = React.useState<string | null>(null);
+  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState<string | null>(null);
+  const [passwordConfirmErrorMessage, setPasswordConfirmErrorMessage] = React.useState<string | null>(null);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
   const handleClickShowPasswordConfirm = () => setShowPasswordConfirm((show) => !show);
@@ -26,8 +32,74 @@ export default function LoginForm() {
     event.preventDefault();
   };
 
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+    if (!email) {
+      setEmailErrorMessage("Email is required.");
+    } else if (!emailRegex.test(email)) {
+      setEmailErrorMessage("Please enter a valid email address.");
+    } else {
+      setEmailErrorMessage(null);
+      return true;
+    }
+
+    return false;
+  };
+
+  const validatePassword = (password: string) => {
+    const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).*$/;
+
+    if (!password) {
+      setPasswordErrorMessage("Password is required.");
+    } else if (password.length < 8 || password.length > 24) {
+      setPasswordErrorMessage("Password must be 8-24 characters long.");
+    } else if (!passwordRegex.test(password)) {
+      setPasswordErrorMessage("Password must include at least 1 lowercase character, uppercase character, symbol and number.");
+    } else {
+      setPasswordErrorMessage(null);
+      return true;
+    }
+
+    return false;
+  }
+
+  const validatePasswordConfirm = (passwordConfirm: string) => {
+    if (password != passwordConfirm) {
+      setPasswordConfirmErrorMessage("Password and Confirm Password do not match.")
+    } else {
+      setPasswordConfirmErrorMessage(null);
+      return true;
+    }
+
+    return false;
+  }
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    const isValid = validateEmail(e.target.value);
+
+    setDisableSubmit(!isValid || passwordErrorMessage !== null || passwordConfirmErrorMessage !== null);
+  }
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+    const isValid = validatePassword(e.target.value);
+    setDisableSubmit(emailErrorMessage !== null || !isValid || passwordConfirmErrorMessage !== null);
+  }
+
+  const handlePasswordConfirmChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordConfirm(e.target.value)
+    const isValid = validatePasswordConfirm(e.target.value);
+    setDisableSubmit(emailErrorMessage !== null || passwordErrorMessage !== null || !isValid);
+  }
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (validateEmail(email) && validatePassword(password) && validatePasswordConfirm(passwordConfirm)) {
+            
+    }
   }
 
   return (
@@ -37,16 +109,20 @@ export default function LoginForm() {
       onSubmit={handleSubmit}
     >
       <TextField
+        required
         id="signup-email"
         label="Email"
         variant="outlined"
         fullWidth={true}
         value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={handleEmailChange}
+        error={emailErrorMessage !== null}
+        helperText={emailErrorMessage}
         sx={{ mb: 2 }}
       />
       
       <FormControl
+        required
         variant="outlined"
         fullWidth={true}
         sx={{ mb: 2 }}
@@ -56,7 +132,8 @@ export default function LoginForm() {
           id="signup-password"
           type={showPassword ? 'text' : 'password'}
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={handlePasswordChange}
+          error={passwordErrorMessage !== null}
           endAdornment={
             <InputAdornment position="end">
               <IconButton
@@ -74,9 +151,13 @@ export default function LoginForm() {
           }
           label="Password"
         />
+        {passwordErrorMessage !== null && (
+          <FormHelperText error={true}>{passwordErrorMessage}</FormHelperText>
+        )}
       </FormControl>
       
       <FormControl
+        required
         variant="outlined"
         fullWidth={true}
         sx={{ mb: 2 }}
@@ -86,7 +167,8 @@ export default function LoginForm() {
             id="signup-password-confirm"
             type={showPasswordConfirm ? 'text' : 'password'}
             value={passwordConfirm}
-            onChange={(e) => setPasswordConfirm(e.target.value)}
+            onChange={handlePasswordConfirmChange}
+            error={passwordConfirmErrorMessage !== null}
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
@@ -104,11 +186,15 @@ export default function LoginForm() {
             }
             label="Password"
           />
+          {passwordConfirmErrorMessage !== null && (
+            <FormHelperText error={true}>{passwordConfirmErrorMessage}</FormHelperText>
+          )}
         </FormControl>
 
         <Button
           type="submit"
           variant="contained"
+          disabled={disableSubmit}
           sx={{ width: '100%', mt: 2 }}
         >
           Register
