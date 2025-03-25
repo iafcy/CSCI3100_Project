94263a85ce975c403ad8e99f12848f8547e21745
Iafcy622 <frankiecheung512@gmail.com>
2025-03-25 23:14:55 +0800

    Updated return type for endpoints

--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -1,15 +1,21 @@
 export type Thread = {
   id: number;
+  title: string;
+  userId: number;
   username: string;
-  title: string | null;
   categoryId: number;
+  like: number;
+  dislike: number;
+  createdAt: Date;
 }
 
 export type Comment = {
   id: number;
-  threadId: number;
+  userId: number;
   username: string;
+  threadId: number;
+  content: string;
   like: number;
   dislike: number;
-  content: string;
+  createdAt: Date;
 }
