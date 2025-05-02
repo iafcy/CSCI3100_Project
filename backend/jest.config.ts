83f262d7b5ae7891a188085bb53dbb1248ac131b
Iafcy622 <frankiecheung512@gmail.com>
2025-05-02 17:34:09 +0800

    Updated tsconfig to avoid conflict

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,6 +1,10 @@
-module.exports = {
+import type { Config } from 'jest';
+
+const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js'],
 };
+
+export default config;
